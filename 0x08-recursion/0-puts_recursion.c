e27d4cb2fa7b404e67cc00c9879a8da6ddca10e5
Mekdes Dereje <yayesh1316@gmail.com>
2023-07-06 09:14:40 -0700

    deep inside herself

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
+void _puts_recursion(char *s)
+{
+	if (*)
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar('\n');
+}
