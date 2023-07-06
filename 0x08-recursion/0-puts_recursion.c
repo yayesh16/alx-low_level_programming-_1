ca1a62203368031ab40647f795d8706b99f24ac5
Mekdes Dereje <yayesh1316@gmail.com>
2023-07-06 09:44:28 -0700

    deep inside herself

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,16 +1,18 @@
 #include "main.h"
 /**
- * _puts_recursion - function like puts();
- * @s: input
- * Return: Always 0 (Success)
+ * _puts_recursion - Print a string followed by a new line
+ * @s:the string to print
+ *
+ * Return: Nothing.
  */
 void _puts_recursion(char *s)
 {
-	if (*)
+	if (*s == '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
-	}
-	else
 		_putchar('\n');
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
 }
