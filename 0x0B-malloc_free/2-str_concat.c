23152583ce07ca3d3ad3894332fc5971686234de
Mekdes Dereje <yayesh1316@gmail.com>
2023-07-10 13:28:31 -0700

    He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,7 +14,7 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		s2 == "";
+		s2 = "";
 
 	i = ci = 0;
 	while (s1[i] != '\0')
@@ -29,11 +29,11 @@ char *str_concat(char *s1, char *s2)
 	while (s1[i] != '\0')
 	{
 		conct[i] = s1[i];
-		i++
+		i++;
 	}
 	while (s2[ci] != '\0')
 	{
-		conct[i] = s2[ci]
+		conct[i] = s2[ci];
 			i++, ci++;
 	}
 	conct[i] = '\0';
